68ac1b4583c0315686973edf6458168eb2e53060
DimaBuno <skifi11legion@gmail.com>
2017-12-11 20:23:36 +0200

    Added entities and models.

--- AppointmentManager/AppointmentManager.DataAccess/UserRepository.cs
+++ AppointmentManager/AppointmentManager.DataAccess/UserRepository.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using AppointmentManager.DataAccess.Entities;
 
+
+
 namespace AppointmentManager.DataAccess
 {
     public class UserRepository : Repository<User>
@@ -15,8 +17,8 @@ namespace AppointmentManager.DataAccess
         {
             //TODO: Replace with real database.
             return new List<User> {
-                new User { FirstName = "User", LastName = "1" },
-                new User { FirstName = "User", LastName = "2" }
+                new User { UserId= "1", DateBday ="01.01.2000", Email="123@gmail.com", GenderId = "1", Login ="User1", Password="123456", Role="Admin",  FirstName = "John", LastName = "1" },
+                new User { UserId= "2", DateBday ="02.02.2001", Email="abc@gmail.com", GenderId = "2", Login ="User2", Password="123456", Role="Employee",  FirstName = "Ann", LastName = "1" }
             };
         }
     }
---
+++ AppointmentManager/AppointmentManager.Service/Models/Orders.cs
@@ -0,0 +1,11 @@
+﻿namespace AppointmentManager.Service.Models
+{
+    public class Appointments
+    {
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public string UserIdCustumer { get; set; }
+        public string UserIdEmployee { get; set; }
+        public string StatusId { get; set; }
+    }
+}
